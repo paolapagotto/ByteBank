1bf1bd5e5da03511e53046223bbdcea507e1678d
paolapagotto <paolapagotto@gmail.com>
2020-09-21 19:03:35 +0100

    cria nova classe e método para conta

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-
-//clientes do Banco
-const cliente1Nome = 'Ricardo';
-const cliente1CPF = 11122233304;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
-
-const cliente2Nome = 'Alice';
-const cliente2CPF = 55522233304;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
-
-console.log(cliente2Nome);
-
-
-//criação de classe para Cliente e suas propriedades
 class Cliente {
 
     nome;
     cpf;
-    agencia;
-    saldo;
 
 }
 
-//instanciando a classe Cliente
+class ContaCorrente {
+
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
+
+
+
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
 
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 11122233304;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-//comand cmd + alt = multiplos cursores
+const cliente2 = new Cliente();
+
 cliente2.nome = 'Alice';
 cliente2.cpf = 55522233304;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+//alterando saldo de cliente
+const contaCorrenteCliente1 = new ContaCorrente();
+contaCorrenteCliente1.saldo = 0;
+contaCorrenteCliente1.agencia = 1001;
+console.log(contaCorrenteCliente1.saldo);
+contaCorrenteCliente1.saldo = 100;
+console.log(contaCorrenteCliente1.saldo);
+
+//criando método para classe
+contaCorrenteCliente1.sacar(30);
+console.log(contaCorrenteCliente1.saldo);
 
 console.log(cliente1, cliente2);
 
