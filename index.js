5e987e35a124e972655213381e96e1997d8d432e
paolapagotto <paolapagotto@gmail.com>
2020-09-21 19:29:18 +0100

    métodos com retorno

--- index.js
+++ index.js
@@ -14,49 +14,37 @@ class ContaCorrente {
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor;
-            console.log(this._saldo);
+            return valor;
         }
     }
 
     depositar(valor){
         if(valor > 0){
             this._saldo += valor
-            console.log(this._saldo);
+            return valor;
         }
     }
 }
-//campos privados: # ou _ private field
-
-
 
 const cliente1 = new Cliente();
 
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 11122233304;
 
-const cliente2 = new Cliente();
-
-cliente2.nome = 'Alice';
-cliente2.cpf = 55522233304;
-
 //alterando saldo de cliente
 const contaCorrenteCliente1 = new ContaCorrente();
-contaCorrenteCliente1.saldo = 0;
 contaCorrenteCliente1.agencia = 1001;
-console.log(contaCorrenteCliente1.saldo);
-
-contaCorrenteCliente1.saldo += 100;
-console.log(contaCorrenteCliente1.saldo);
 
 //criando métodos para classe
 //sacar:
 contaCorrenteCliente1.sacar(30);
-console.log(contaCorrenteCliente1.saldo);
+
 //depositar:
 contaCorrenteCliente1.depositar(-20);
-console.log(contaCorrenteCliente1.saldo);
+
+const valorDepositado = contaCorrenteCliente1.depositar(50);
+const valorSacado = contaCorrenteCliente1.sacar(30);
 
 console.log(contaCorrenteCliente1);
-console.log(cliente1, cliente2);
 
 
