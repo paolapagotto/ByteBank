f56208ce0876051320afd51c98e7a68492bc2388
paolapagotto <paolapagotto@gmail.com>
2020-09-21 19:44:15 +0100

    cria módulos js

---
+++ ContaCorrente.js
@@ -0,0 +1,20 @@
+class ContaCorrente {
+
+    agencia;
+    _saldo = 0;
+    //#saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this._saldo += valor
+            return valor;
+        }
+    }
+}
