644737bd5c723fc181d45f9491a465ba14ce67b8
paolapagotto <paolapagotto@gmail.com>
2020-09-22 14:16:52 +0100

    cria Sistema de Autenticação com polimorfismo

--- funcionarios/Gerente.js
+++ funcionarios/Gerente.js
@@ -4,7 +4,6 @@ export class Gerente extends Funcionario {
     constructor(nome, cpf, salario){
         super(nome, cpf, salario)
 
-
         this._bonificacao = 1.1;
     }
 }
--- index.js
+++ index.js
@@ -4,6 +4,24 @@ import { ContaCorrente } from './conta/ContaCorrente.js';
 import { ContaPoupanca } from './conta/ContaPoupanca.js';
 import { ContaSalario } from './conta/ContaSalario.js';
 
+import { Gerente } from './funcionarios/Gerente.js'
+import { Diretor } from './funcionarios/Diretor.js'
+import { SistemaAutenticacao } from './SistemaAutenticacao.js'
+
+//Funcionarios
+
+const diretor = new Diretor('Rodrigo', 11111122201, 10000);
+diretor.cadastrarSenha('123456');
+const gerente = new Gerente('Mara', 99988877701, 5000);
+gerente.cadastrarSenha('12a34b');
+const estaLogado = SistemaAutenticacao.login(diretor, '123456');
+console.log(estaLogado);
+
+
+
+
+
+//Cliente
 
 const cliente1 = new Cliente('Ricardo', 11122233304);
 
@@ -18,14 +36,12 @@ const contaSalarioCliente1 = new ContaSalario(100, cliente1, 1001);
 contaSalarioCliente1.sacar(10);
 //console.log(contaSalarioCliente1);
 
-
-
 contaPoupancaCliente1.depositar(400);
 contaPoupancaCliente1.sacar(100);
-console.log(contaPoupancaCliente1);
+//console.log(contaPoupancaCliente1);
 
 contaCorrenteCliente1.depositar(450);
 contaCorrenteCliente1.sacar(100);
-console.log(contaCorrenteCliente1);
+//console.log(contaCorrenteCliente1);
 
 
